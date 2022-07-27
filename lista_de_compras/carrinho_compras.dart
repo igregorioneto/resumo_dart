5c6c48771c2a4f96ec3d876ad3a0c76d2916d6c4
igregorioneto <igregoriodoneto@outlook.com>
2022-07-27 09:11:21 -0300

    Lista de compras simples

---
+++ lista_de_compras/carrinho_compras.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+List<String> produtos = [];
+
+main() {
+  bool condicao = true;
+
+  while (condicao) {
+    print("====  ADICIONE UM PRODUTO  ====");
+    String text = stdin.readLineSync() ?? "";
+    if (text == "sair") {
+      print("==== TERMINOU O PROGRAMA ====");
+      condicao = false;
+    } else if (text == "imprimir") {
+      imprimir();
+    } else if (text == "remover") {
+      remover();
+    } else {
+      produtos.add(text!);
+      print("\x1B[2J\x1B[0;0H");
+    }
+  }
+}
+
+imprimir() {
+  for (var i = 0; i < produtos.length; i++) {
+    print("ITEM $i - ${produtos[i]}");
+  }
+}
+
+remover() {
+  print("==== Qual item deseja remover?");
+  String input = stdin.readLineSync() ?? "";
+  int item = int.parse(input);
+  produtos.removeAt(item);
+  print("==== ITEM REMOVIDO ====");
+}
